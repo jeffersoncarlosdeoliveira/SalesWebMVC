85d1e364dba59ab3fd97b3b51305fe73a28de6ca
Jefferson <jeffersoncarlosdeoliveira@outlook.com>
2020-10-26 16:01:48 -0300

    Implementing simple Search

--- SalesWebMVC/Models/SalesRecord.cs
+++ SalesWebMVC/Models/SalesRecord.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SalesWebMVC.Models.Enums;
+using SalesWebMVC.Data;
+using System.ComponentModel.DataAnnotations;
 
 namespace SalesWebMVC.Models
 {
     public class SalesRecord
     {
         public int Id { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Amout { get; set; }
         public SalesStatus Status { get; set; }
         public Seller Seller { get; set; }
